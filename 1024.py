518a35eacfc50874ff42d0662578dd38ea397eab
mack56 <78888640+mack56@users.noreply.github.com>
2021-05-28 13:10:49 +0800

    Update 1024.py

--- 1024.py
+++ 1024.py
@@ -182,7 +182,7 @@ class Autoreply:
     @staticmethod
     def getreply():
         #自定义回复内容，记得修改随机数
-        reply=['感谢分享','感谢你的分享','谢谢分享','多谢分享','感谢作者的分享','谢谢坛友分享','感谢分享']
+        reply=['感谢分享','感谢你的分享','谢谢分享','多谢分享','感谢坛友分享','谢谢坛友分享','感谢分享']
         reply_m=random.randint(0,6)
         reply_news=reply[reply_m]
         print('本次回复消息是:'+reply_news) #print('本次回复消息是:'+reply_news)
@@ -259,7 +259,7 @@ class Autoreply:
     def main(cookieslist,todaylist,ge):
         #回复
         n=0
-        sl=random.randint(55,89)
+        sl=random.randint(33,66)
         print('本次需回复'+str(sl)+'个')
         cookies=cookieslist[ge]
         m=Autoreply.getnumber(cookies)
@@ -272,7 +272,7 @@ class Autoreply:
                 geturl,tid=Autoreply.getonelink(todaylist)
                 reply_news=Autoreply.getreply()
                 res=Autoreply.getmatch(geturl,cookies)
-                sleeptime=random.randint(90,280)
+                sleeptime=random.randint(150,400)
                 au=Autoreply.postreply(cookies,res,reply_news,tid)
                 if au=='回复成功':
                     print('第'+str(ge+1)+'个账号回复成功')
